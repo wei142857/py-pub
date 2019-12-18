97653df69edb5a7ff23f600cf4294b03d8ae6ccf
wei142857 <weiqz142857@163.com>
2019-12-18 17:40:56 +0800

    测试分支

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class GameAssist:
 		self.im2num_arr = []
 
 		# 主截图的左上角坐标和右下角坐标
-		self.scree_left_and_right_point = (577, 289, 1045, 601) # 手动更改
+		self.scree_left_and_right_point = (577, 289, 1045, 601) # 手动更改2
 		# 小图标宽高
 		self.im_width = 39
 
